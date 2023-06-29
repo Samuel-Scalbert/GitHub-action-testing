cfa5af07c088d6d4e46a3a7dcaa9e63b7ec770d3
Scalbert Samuel <32683708+Samuel-Scalbert@users.noreply.github.com>
2023-06-29 15:40:28 +0200

    Update xml_check.py

--- .github/workflows/xml_check.py
+++ .github/workflows/xml_check.py
@@ -13,7 +13,7 @@ def check_xml_files():
         try:
             tree = etree.parse(file_path)
             root = tree.getroot()
-            if root.tag != "tei":
+            if root.tag != "TEI":
                 raise Exception(f"File {file_path} does not contain a <tei> tag")
         except Exception as e:
             print(f"Error: {str(e)}", file=sys.stderr)
