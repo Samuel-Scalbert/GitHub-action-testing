5fbec5a271f49999375d794b5d6bf92acb8b8a71
Scalbert Samuel <32683708+Samuel-Scalbert@users.noreply.github.com>
2023-06-29 16:23:20 +0200

    Update xml_check.py

--- .github/workflows/xml_check.py
+++ .github/workflows/xml_check.py
@@ -1,20 +1,24 @@
 import os
 import sys
 from lxml import etree
-from github import Github, GithubException
-import json
+import subprocess
 
-def check_xml_files(files):
-    for file_path in files:
+def check_xml_files():
+    xml_files = []
+    for root, dirs, files in os.walk(".", topdown=False):
+        for file in files:
+            if file.endswith(".xml"):
+                xml_files.append(os.path.join(root, file))
+
+    for file_path in xml_files:
         try:
             tree = etree.parse(file_path)
             root = tree.getroot()
-            if root.tag != "tei":
+            if root.tag != "{http://www.tei-c.org/ns/1.0}TEI":
                 raise Exception(f"File {file_path} does not contain a <tei> tag")
         except Exception as e:
-            print(f"Error: {str(e)}", file=sys.stderr)
-            sys.exit(1)  # Exit with a non-zero code
+            print(f"Error: {str(e)}")
+    print("All XML files contain a <tei> tag.")
 
 if __name__ == "__main__":
-    files = json.loads(sys.argv[1])
-    check_xml_files(files)
+    check_xml_files()
