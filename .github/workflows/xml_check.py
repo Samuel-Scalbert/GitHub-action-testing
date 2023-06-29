9e54ce6751e67f3daf4263350ee022fad339acde
Scalbert Samuel <32683708+Samuel-Scalbert@users.noreply.github.com>
2023-06-29 15:15:26 +0200

    Update xml_check.py

--- .github/workflows/xml_check.py
+++ .github/workflows/xml_check.py
@@ -1,9 +1,21 @@
 import os
 import sys
-from lxml import etree
+from bs4 import BeautifulSoup
 
 def check_xml_files():
- 
+    xml_files = []
+    for root, dirs, files in os.walk(".", topdown=False):
+        for file in files:
+            if file.endswith(".xml"):
+                xml_files.append(os.path.join(root, file))
+
+    for file_path in xml_files:
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                content = file.read()
+                soup = BeautifulSoup(content, "xml")
+                if not soup.find("tei"):
+                    raise Exception(f"File {file_path} does not contain a <tei> tag")
         except Exception as e:
             print(f"Error: {str(e)}", file=sys.stderr)
             sys.exit(1)  # Exit with a non-zero code
