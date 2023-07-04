2f958dae35b5b3fe91b74f44424be2f370b17127
Scalbert Samuel <32683708+Samuel-Scalbert@users.noreply.github.com>
2023-07-04 10:47:19 +0200

    Update xml_check.py

--- .github/workflows/xml_check.py
+++ .github/workflows/xml_check.py
@@ -1,22 +1,24 @@
 import sys
+from bs4 import BeautifulSoup
 
-def get_first_line(file_path):
+def has_tags(file_path, tags):
     try:
         with open(file_path, "r") as file:
-            # Skip the first line if it starts with '<?xml'
-            first_line = next(file).strip()
-            if first_line.startswith('<?xml'):
-                first_line = next(file).strip()
-            return first_line
+            soup = BeautifulSoup(file, "xml")
+            for tag in tags:
+                if soup.find(tag):
+                    return tag
+            return False
     except Exception as e:
         error_message = str(e)
         return error_message
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please provide the XML file path as a command-line argument.")
+    if len(sys.argv) < 3:
+        print("Please provide the XML file path and tags as command-line arguments.")
         sys.exit(1)
 
     file_path = sys.argv[1]
-    first_line = get_first_line(file_path)
-    print(first_line)
+    tags = ['p']
+    result = has_tags(file_path, tags)
+    print(result)
